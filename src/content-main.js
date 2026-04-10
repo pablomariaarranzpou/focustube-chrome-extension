8566ad877403788c70604a2494e88e973bc335c9
Pablo María Arranz Pou <pablomariaarranzpou@gmail.com>
2026-04-10 20:50:39 +0200

    version writting

--- src/content-main.js
+++ src/content-main.js
@@ -35,7 +35,7 @@
 // <script src="src/features/HideBlacklistedChannelsFeature.js"></script>
 // <script src="src/features/HideBlacklistedWordsFeature.js"></script>
 
-console.log('FocusTube content script loaded - Version 2026-01-18 (Refactored)');
+console.log('FocusTube content script loaded - Version 2.4.7');
 
 // Global feature manager
 let featureManager = null;
